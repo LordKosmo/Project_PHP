1d888a449eacb453d47387e7f1b1a1d08becd515
Alexis Bugielski <alexis.bugielski@gmail.com>
2018-04-07 00:41:04 +0200

    Adding html pages

--- C/userController.php
+++ C/userController.php
@@ -1,3 +1,22 @@
 <?php
-/*code of user*/
+
+function accueil(){
+
+	//require ("V/accueil.html");
+	header('Location: V/accueil.html');
+  //exit();
+
+}
+
+
+function verifUtilisateur(){
+	$login=isset($_POST['Login'])?$_POST['Login']:"tapez votre login";
+	$passe=isset($_POST['Password'])?$_POST['Password']:"tapez votre passe";
+	require ("M/Ok.php");
+	if (verifLoginBd($login, $passe)) 
+		echo ("ok");
+	else 
+		echo ("ko");
+}
+
 ?>
